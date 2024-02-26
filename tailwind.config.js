533ffcd5eb575ebc11eafc5f43a283dd9d030cea
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-25 21:18:34 -0600

    :wrench: Configure color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Color palette acquired at https://colorhunt.co/palette/f7f7f7eeeeee393e46929aab
+        primary: "#F7F7F7",
+        secondary: "#EEEEEE",
+        tertiary: "#393E46",
+        quaternary: "#929AAB",
+      },
+    },
   },
   plugins: [],
 };
