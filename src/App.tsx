ed0431a432a78e7a1e6e3279dda32c209310b5fb
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-26 00:33:13 -0600

    :sparkles: Added BrowserRouter and Routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
