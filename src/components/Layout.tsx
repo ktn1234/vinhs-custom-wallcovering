ac61eaadfbadbed9e46e53872687db13863c87da
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-26 00:32:02 -0600

    :sparkles: Created Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
+
+export default Layout;
